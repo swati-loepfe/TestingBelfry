3ec02d8c5021d21b7d26d27983366631a1649a62
gus <swati.gupta@loepfe.com>
2024-03-25 09:04:44 +0100

    Merge remote-tracking branch 'origin/main'

--- TestingProject/pageObjects/monitoring_page.py
+++ TestingProject/pageObjects/monitoring_page.py
@@ -5,15 +5,25 @@ class MonitoringPage:
     #Main Search/Monitoring button
     search_button = (By.XPATH,"/html/body/app-root/app-shell/div[1]/div[2]/app-navbar/div/span[2]")
     search_compare_button = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/app-tab-bar/div/span[1]")
+    search_compare_lastcell1=(By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-compare/div/div[2]/div[1]/loe-dashboard-tile/div/div[2]/app-nslt-matrix/div/div[2]/div[6]/div[1]/span/span")
+    search_compare_lastcell2 = (By.XPATH,
+                                "/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-compare/div/div[2]/div[1]/loe-dashboard-tile/div/div[2]/app-nslt-matrix/div/div[2]/div[7]/div[1]/span/span")
+    search_compare_lastcell3 = (By.XPATH,
+                                "/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-compare/div/div[2]/div[1]/loe-dashboard-tile/div/div[2]/app-nslt-matrix/div/div[2]/div[8]/div[1]/span/span")
     search_NSLT_button = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/app-tab-bar/div/span[2]")
     absolute_button = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-compare/div/div[1]/loe-select/div/span[1]")
     event_button=(By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/app-tab-bar/div/span[5]")
     splice_button =(By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/app-tab-bar/div/span[3]")
     bad_positions = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/app-tab-bar/div/span[4]")
+    bad_position_cv = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-bad-positions/div/div[2]/table/thead/tr/th[5]/div")
     event_log_title=(By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-event-log/div/loe-dashboard-tile/div/loe-dashboard-tile/div/div")
     search_compare_header = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-compare/div/div[2]/div[1]/loe-dashboard-tile/div/div[1]")
     search_compare_mainheader = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-event-log/div/loe-dashboard-tile/div/div")
     search_compare_header_article2 = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-compare/div/div[2]/div[2]/loe-dashboard-tile/div/div[1]")
+    compare_produced_length = (By.XPATH,"/div/span[2]")
+    event__arrow_up = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-event-log/div/loe-dashboard-tile/div/div/div/span[2]")
+    event_bar_down = (By.XPATH,"/div/div[2]/loe-machine-position-chart/div/div[68]/div[1]/div")
+    event_Run_Inhibit =(By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-monitoring/div/div[1]/div/div/app-monitoring-event-log/div/loe-dashboard-tile/div/loe-dashboard-tile/div/cdk-virtual-scroll-viewport/div[1]/div[11]")
 
 
     def __init__(self,driver):
@@ -62,3 +72,36 @@ class MonitoringPage:
     def return_compare_header_article2(self):
         driver = self.driver
         return driver.find_element(*MonitoringPage.search_compare_header_article2)
+
+    def return_compare_produced_length(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.compare_produced_length)
+
+    def return_event_arrow(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.event__arrow_up)
+
+    #Change when GUI ID work
+    def return_event_bardown(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.event_bar_down)
+
+    def return_event_run_inhibit(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.event_Run_Inhibit)
+
+    def return_compare_lastcell1(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.search_compare_lastcell1)
+
+    def return_compare_lastcell2(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.search_compare_lastcell2)
+
+    def return_compare_lastcell3(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.search_compare_lastcell3)
+
+    def return_bad_position_cv(self):
+        driver = self.driver
+        return driver.find_element(*MonitoringPage.bad_position_cv)
--- TestingProject/testCases/test_e2eBelfry.py
+++ TestingProject/testCases/test_e2eBelfry.py
@@ -1,7 +1,5 @@
 import time
-
 import pytest
-import allure
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -14,8 +12,9 @@ from testCases.BaseClass import BaseClass
 
 
 @pytest.mark.usefixtures("setup", "log_on_failure")
-class TestLogin(BaseClass):
+class TestLogin:
     baseURL = "http://13.68.159.147/"
+    #baseURL="http://localhost/"
 
 
 def test_openPage(setup):
@@ -23,34 +22,35 @@ def test_openPage(setup):
     driver.get(TestLogin.baseURL)
     driver.maximize_window()
     driver.implicitly_wait(10)
-    driver.close()
+    #driver.close()
 
 
 def test_navigate_machinedashboard(setup):
     driver = setup
     driver.get(TestLogin.baseURL)
     driver.maximize_window()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(20)
     screenpage = ScreensaverPage(driver)
     screenpage.screensaver_button().click()
-
+    #driver.close()
 
 def test_navigate_articledashboard(setup):
     driver = setup
     driver.get(TestLogin.baseURL)
     driver.maximize_window()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(20)
     screenpage = ScreensaverPage(driver)
     screenpage.screensaver_button().click()
     articlepage = ArticlesPage(driver)
-    time.sleep(2)
-    articlepage.article_button().click()
+    time.sleep(3)
+    articlepage.return_article_button().click()
+    #driver.close()
 
 def test_login_page_correctpassword(setup):
     driver = setup
     driver.get(TestLogin.baseURL)
     driver.maximize_window()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(20)
     screenpage = ScreensaverPage(driver)
     screenpage.screensaver_button().click()
     loginpage=LoginPage(driver)
@@ -60,12 +60,13 @@ def test_login_page_correctpassword(setup):
     loginpage.return_set_password().send_keys(123)
     loginpage.return_login_check1().click()
     loginpage.return_login_check2().click()
+    #driver.close()
 
 def test_login_page_incorrectpassword(setup):
     driver = setup
     driver.get(TestLogin.baseURL)
     driver.maximize_window()
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(20)
     screenpage = ScreensaverPage(driver)
     screenpage.screensaver_button().click()
     loginpage=LoginPage(driver)
@@ -75,7 +76,7 @@ def test_login_page_incorrectpassword(setup):
     loginpage.return_set_password().send_keys(1234)
     loginpage.return_login_check1().click()
     loginpage.return_login_check2().click()
-    time.sleep(10)
+    time.sleep(20)
     loginpage.return_incorrect_password().click()
     #invoke virtual keyboard again
     loginpage.return_login_password().click()
@@ -83,7 +84,7 @@ def test_login_page_incorrectpassword(setup):
     loginpage.return_set_password().send_keys(123)
     loginpage.return_login_check1().click()
     loginpage.return_login_check2().click()
-
+    #driver.close()
 
 def test_settings(setup):
     driver = setup
@@ -104,17 +105,18 @@ def test_settings(setup):
     settingspage.settings_button().click()
     time.sleep(10)
     settingspage.settings_button1().click()
-    driver.close()
+    #driver.close()
 
-def test_monitoring_click_alltabs(setup):
+
+def test_monitoring_navigate_alltabs_monitoringtab(setup):
     driver = setup
     driver.get(TestLogin.baseURL)
     driver.maximize_window()
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(30)
     screenpage = ScreensaverPage(driver)
     screenpage.screensaver_button().click()
     searchpage=MonitoringPage(driver)
-    time.sleep(10)
+    time.sleep(5)
     searchpage.return_search_button().click()
     time.sleep(5)
     searchpage.return_NSLT_button().click()
@@ -126,13 +128,14 @@ def test_monitoring_click_alltabs(setup):
     searchpage.return_compare_button().click()
     time.sleep(5)
     searchpage.return_event_button().click()
+    #driver.close()
 
 # This test compares the header titles for Event logs Tab under monitoring dashboard
 def test_Monitoring_Event_log(setup):
     driver = setup
     driver.get(TestLogin.baseURL)
     driver.maximize_window()
-    driver.implicitly_wait(50)
+    driver.implicitly_wait(20)
     # Navigate to Article Dashboard
     screenpage = ScreensaverPage(driver)
     screenpage.screensaver_button().click()
@@ -144,7 +147,10 @@ def test_Monitoring_Event_log(setup):
     monitoring_page.return_event_button().click()
     time.sleep(3)
     assert monitoring_page.return_event_log_title().text == "Event\nTimestamp"
-    assert monitoring_page.return_event_log_maintitle().text == "Article 2 Position-1\nundo\nrefresh"
+    assert monitoring_page.return_event_log_maintitle().text == "Article 1 Position-1\nundo\nrefresh"
+    monitoring_page.return_event_arrow().click()
+    monitoring_page.return_event_run_inhibit().click()
+    #driver.close()
 
 
 # This test compares the header titles for compare monitoring dashboard
@@ -161,8 +167,27 @@ def test_compare_header(setup):
     time.sleep(1)
     monitoring_page.return_search_button().click()
     time.sleep(1)
-    assert monitoring_page.return_compare_header().text == "1 R\nCount alarm 0.00 F%"
+    assert monitoring_page.return_compare_header().text == "1 R\nCount alarm 0.00 Eff.%"
     assert monitoring_page.return_compare_header_article2().text == "Article 1"
+    # Last 3 values of the Grid
+    assert monitoring_page.return_compare_lastcell1().text == "05"
+    assert monitoring_page.return_compare_lastcell2().text == "06"
+    assert monitoring_page.return_compare_lastcell3().text == "07"
+    #driver.close()
 
+def test_monitoring_bad_positions(setup):
+    driver = setup
+    driver.get(TestLogin.baseURL)
+    driver.maximize_window()
+    driver.implicitly_wait(20)
+    screenpage = ScreensaverPage(driver)
+    screenpage.screensaver_button().click()
+    searchpage = MonitoringPage(driver)
+    time.sleep(5)
+    searchpage.return_search_button().click()
+    searchpage.return_bad_positions().click()
+    time.sleep(5)
+    searchpage.return_bad_position_cv().click()
+    #driver.close()
 
 
