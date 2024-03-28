dfae1c4d18effe54e9efd0112653d14fcc35aa29
gus <swati.gupta@loepfe.com>
2024-03-28 11:14:09 +0100

    Merge remote-tracking branch 'origin/main'

--- TestingProject/pageObjects/articles_page.py
+++ TestingProject/pageObjects/articles_page.py
@@ -2,7 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class ArticlesPage:
-    article_button = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-machine-dashboard/div/app-machine-efficiency[1]/loe-dashboard-tile/div/div[2]/loe-dashboard-tile/div/loe-line-chart/div/div")
+    #article_button = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-machine-dashboard/div/app-machine-efficiency[1]/loe-dashboard-tile/div/div[2]/loe-dashboard-tile/div/loe-line-chart/div/div")
+    article_button = (By.XPATH,"/html/body/app-root/app-screen-saver/div[2]/div/div/div/div[1]/div[1]/div[2]")
+    articles_main_button =(By.XPATH,"/html/body/app-root/app-shell/div[1]/div[2]/app-navbar/div/span[3]")
+    articles_edit_article_lower =(By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-article-settings/div[2]/div/table/tbody/tr/td[1]/loe-article-select/div/div/span")
+    articles_slider_button = (By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-article-settings/div[2]/div/table/tbody/tr/td[3]/div/div[1]/loe-switch/label/span")
+    articles_edit_save_changes =(By.XPATH,"/html/body/app-root/app-shell/div[2]/div/app-article-settings/div[1]/div[2]/loe-button/div/div/span")
 
     def __init__(self, driver):
         self.driver = driver
@@ -10,3 +15,19 @@ class ArticlesPage:
     def return_article_button(self):
         driver = self.driver
         return driver.find_element(*ArticlesPage.article_button)
+
+    def return_articles_main_button(self):
+        driver = self.driver
+        return driver.find_element(*ArticlesPage.articles_main_button)
+
+    def return_articles_edit_article_lower(self):
+        driver = self.driver
+        return driver.find_element(*ArticlesPage.articles_edit_article_lower)
+
+    def return_articles_slider_button(self):
+        driver = self.driver
+        return driver.find_element(*ArticlesPage.articles_slider_button)
+
+    def return_articles_save_changes(self):
+        driver = self.driver
+        return driver.find_element(*ArticlesPage.articles_edit_save_changes)
--- TestingProject/testCases/test_e2eBelfry.py
+++ TestingProject/testCases/test_e2eBelfry.py
@@ -25,13 +25,13 @@ def test_openPage(setup):
     #driver.close()
 
 
-def test_navigate_machinedashboard(setup):
-    driver = setup
-    driver.get(TestLogin.baseURL)
-    driver.maximize_window()
-    driver.implicitly_wait(20)
-    screenpage = ScreensaverPage(driver)
-    screenpage.screensaver_button().click()
+#def test_navigate_machinedashboard(setup):
+ #   driver = setup
+  #  driver.get(TestLogin.baseURL)
+   # driver.maximize_window()
+   # driver.implicitly_wait(20)
+    #screenpage = ScreensaverPage(driver)
+    #screenpage.screensaver_button().click()
     #driver.close()
 
 def test_navigate_articledashboard(setup):
@@ -41,9 +41,9 @@ def test_navigate_articledashboard(setup):
     driver.implicitly_wait(20)
     screenpage = ScreensaverPage(driver)
     screenpage.screensaver_button().click()
-    articlepage = ArticlesPage(driver)
-    time.sleep(3)
-    articlepage.return_article_button().click()
+    #articlepage = ArticlesPage(driver)
+    #time.sleep(3)
+    #articlepage.return_article_button().click()
     #driver.close()
 
 def test_login_page_correctpassword(setup):
@@ -76,7 +76,7 @@ def test_login_page_incorrectpassword(setup):
     loginpage.return_set_password().send_keys(1234)
     loginpage.return_login_check1().click()
     loginpage.return_login_check2().click()
-    time.sleep(20)
+    time.sleep(5)
     loginpage.return_incorrect_password().click()
     #invoke virtual keyboard again
     loginpage.return_login_password().click()
@@ -104,7 +104,7 @@ def test_settings(setup):
     time.sleep(10)
     settingspage.settings_button().click()
     time.sleep(10)
-    settingspage.settings_button1().click()
+    #settingspage.settings_button1().click()
     #driver.close()
 
 
@@ -167,7 +167,7 @@ def test_compare_header(setup):
     time.sleep(1)
     monitoring_page.return_search_button().click()
     time.sleep(1)
-    assert monitoring_page.return_compare_header().text == "1 R\nCount alarm 0.00 Eff.%"
+    assert monitoring_page.return_compare_header().text == "1\nCount alarm 0.00 Eff.%"
     assert monitoring_page.return_compare_header_article2().text == "Article 1"
     # Last 3 values of the Grid
     assert monitoring_page.return_compare_lastcell1().text == "05"
@@ -189,5 +189,29 @@ def test_monitoring_bad_positions(setup):
     time.sleep(5)
     searchpage.return_bad_position_cv().click()
     #driver.close()
+
+def test_edit_article(setup):
+    driver = setup
+    driver.get(TestLogin.baseURL)
+    driver.maximize_window()
+    driver.implicitly_wait(20)
+    screenpage = ScreensaverPage(driver)
+    screenpage.screensaver_button().click()
+    time.sleep(5)
+    loginpage=LoginPage(driver)
+    loginpage.return_login_button().click()
+    loginpage.return_login_password().click()
+    loginpage.return_login_keyboard().click()
+    loginpage.return_set_password().send_keys(123)
+    loginpage.return_login_check1().click()
+    loginpage.return_login_check2().click()
+    articlepage= ArticlesPage(driver)
+    time.sleep(5)
+    articlepage.return_articles_main_button().click()
+    time.sleep(5)
+    articlepage.return_articles_edit_article_lower().click()
+    articlepage.return_articles_slider_button().click()
+    articlepage.return_articles_save_changes().click()
+
 
 
